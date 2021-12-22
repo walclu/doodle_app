0d574ca697a33d7d88e358b5a38be8eb35123263
walclu <lukas-walczak@web.de>
2021-12-22 20:24:31 +0100

    Added Reorderable, dismissable functions

--- lib/screens/home/widgets/daily_tasks_widget.dart
+++ lib/screens/home/widgets/daily_tasks_widget.dart
@@ -1,4 +1,5 @@
 import 'package:doodle_app/models/todo.dart';
+import 'package:doodle_app/screens/projectPage/widgets/todo_form.dart';
 import 'package:doodle_app/services/data_base.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -52,11 +53,18 @@ class _DailyTasksWidgetState extends State<DailyTasksWidget> {
       child: MediaQuery.removePadding(
         context: context,
         removeTop: true,
-        child: ListView.builder(
+        child: ReorderableListView.builder(
+          onReorder: (oldIndex, newIndex) async {
+
+          },
           itemCount: dailyTasks.length,
           itemBuilder: (context, index) {
             int projNum = dailyTasks[index]['projNum'];
             return GestureDetector(
+              key: ValueKey(dailyTasks[index]['todo']),
+              onLongPress: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => TodoForm(project: projects[projNum], index: index)));
+              },
               onTap: () async {
                 int projNum = dailyTasks[index]['projNum'];
                 int todoNum = dailyTasks[index]['todoNum'];
@@ -75,24 +83,67 @@ class _DailyTasksWidgetState extends State<DailyTasksWidget> {
 
 
               },
+
               child: Dismissible(
+                direction: DismissDirection.endToStart,
                 key: ValueKey(dailyTasks[index]['todo']),
+                background: Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.redAccent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("Delete",
+                        style: GoogleFonts.openSans(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 onDismissed: (direction) async {
                   int projNum = dailyTasks[index]['projNum'];
                   int todoNum = dailyTasks[index]['todoNum'];
                   List<Todo> temp = projects[projNum].todos;
+                  Todo safe = projects[projNum].todos[todoNum];
                   temp.removeAt(todoNum);
                   dailyTasks.removeAt(index);
                   await DataBaseService(uid: user.uid).updateProject(
-                      projects[projNum].name,
-                      projects[projNum].done,
-                      temp,
-                      projects[projNum].userPermissions,
-                      projects[projNum].color,
+                    projects[projNum].name,
+                    projects[projNum].done,
+                    temp,
+                    projects[projNum].userPermissions,
+                    projects[projNum].color,
                   );
                   setState(() {
 
                   });
+                  final snackBar = SnackBar(
+                    content: const Text('Task deleted'),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () async {
+                        temp.insert(todoNum, safe);
+                        await DataBaseService(uid: user.uid).updateProject(
+                          projects[projNum].name,
+                          projects[projNum].done,
+                          temp,
+                          projects[projNum].userPermissions,
+                          projects[projNum].color,
+                        );
+                        setState(() {
+
+                        });
+                      },
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+
                 },
                 child: Container(
                   margin: EdgeInsets.only(bottom: 8),
@@ -104,7 +155,7 @@ class _DailyTasksWidgetState extends State<DailyTasksWidget> {
                   child: Row(
                     children: [
                       Icon(Icons.circle_outlined,
-                      color: Color(projects[projNum].color),
+                        color: Color(projects[projNum].color),
                       ),
                       SizedBox(width: 20,),
                       Text(dailyTasks[index]['todo']!.name,
--- lib/screens/projectPage/widgets/todo_overview_widget.dart
+++ lib/screens/projectPage/widgets/todo_overview_widget.dart
@@ -19,16 +19,6 @@ class TodoOverView extends StatefulWidget {
 
 class _TodoOverViewState extends State<TodoOverView> {
 
-  // Future<void> _showTodoPanel(context, Project project, int index) async {
-  //   showModalBottomSheet(
-  //       context: context,
-  //       builder: (context) {
-  //         return Container(
-  //           padding: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
-  //           child: TodoForm(project: project, index: index,),
-  //         );
-  //       });
-  // }
   @override
   Widget build(BuildContext context) {
     final projects = Provider.of<List<Project>?>(context) ?? [];
@@ -36,6 +26,137 @@ class _TodoOverViewState extends State<TodoOverView> {
     final project = projects[widget.index];
     final todos = project.todos;
     return Container(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: ReorderableListView.builder(
+          onReorder: (oldIndex, newIndex) async {
+            setState(() {
+              final index = newIndex > oldIndex ? newIndex - 1 : newIndex;
+              final todo = todos.removeAt(oldIndex);
+              todos.insert(index, todo);
+            });
+            await DataBaseService(uid: user!.uid).updateProject(
+                project.name,
+                project.done,
+                todos,
+                project.userPermissions,
+                project.color
+            );
+          },
+          itemCount: todos.length,
+          itemBuilder: (context, it) {
+            return GestureDetector(
+              key: ValueKey(todos[it]),
+              onDoubleTap: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => TodoForm(project: project, index: it)));
+              },
+              onTap: () async {
+                todos[it].state = !todos[it].state;
+                await DataBaseService(uid: user!.uid).updateProject(
+                    project.name,
+                    project.done,
+                    todos,
+                    project.userPermissions,
+                    project.color
+                );
+                setState(() {
+
+                });
+              },
+
+              child: Dismissible(
+                key: ValueKey(todos[it]),
+                direction: DismissDirection.endToStart,
+                background: Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.redAccent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("Delete",
+                        style: GoogleFonts.openSans(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onDismissed: (direction) async {
+                  Todo safe = projects[widget.index].todos[it];
+                  todos.removeAt(it);
+                  await DataBaseService(uid: user!.uid).updateProject(
+                      project.name,
+                      project.done,
+                      todos,
+                      project.userPermissions,
+                      project.color
+                  );
+                  setState(() {
+
+                  });
+                  final snackBar = SnackBar(
+                    content: const Text('Task deleted'),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () async {
+                        todos.insert(it, safe);
+                        await DataBaseService(uid: user.uid).updateProject(
+                            project.name,
+                            project.done,
+                            todos,
+                            project.userPermissions,
+                            project.color
+                        );
+                        setState(() {
+
+                        });
+                      },
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+
+                },
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.circle_outlined,
+                        color: Color(project.color),
+                      ),
+                      SizedBox(width: 20,),
+                      Text(project.todos[it].name,
+                        style: GoogleFonts.openSans(
+                          decoration: project.todos[it].state? TextDecoration.lineThrough : null,
+                          fontSize: 13,
+                          letterSpacing: 1.2,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+
+
+    /*Container(
       height: MediaQuery
           .of(context)
           .size
@@ -121,6 +242,6 @@ class _TodoOverViewState extends State<TodoOverView> {
           );
         },
       ),
-    );
+    );*/
   }
 }
